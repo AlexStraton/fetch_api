3bd1e33c0fae69ba17c636d0c910da1aa40670e3
Alex Straton <alexandrastraton1@gmail.com>
2024-09-15 18:00:54 +0100

    displayed book title, author, themes and download count on page

--- fetch_books/src/App.jsx
+++ fetch_books/src/App.jsx
@@ -18,7 +18,32 @@ function App() {
     fetchBooks();
   }, []);
 
-  return <></>;
+  return (
+    <>
+      <div> AMAZING BOOKS</div>
+      {books.map((book, i) => {
+        return (
+          <ul key={i}>
+            <div>Title: {book.title}</div>
+            <div>
+              Author:{" "}
+              {book.authors.map((author) => {
+                return author.name;
+              })}
+            </div>
+            <div>
+              Book themes:{" "}
+              {book.bookshelves
+                .filter((theme) => theme.startsWith("Browsing: "))
+                .map((theme) => theme.replace("Browsing: ", ""))
+                .join(", ")}
+            </div>
+            <div>Number of downloads: {book.download_count}</div>
+          </ul>
+        );
+      })}
+    </>
+  );
 }
 
 export default App;
