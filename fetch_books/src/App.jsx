c021a291d512efcadca916b14df7a4e26c73676c
Alex Straton <alexandrastraton1@gmail.com>
2024-09-15 18:41:42 +0100

    styled the cards using MUI

--- fetch_books/src/App.jsx
+++ fetch_books/src/App.jsx
@@ -1,6 +1,15 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import getBooks from "../src/api";
+import * as React from "react";
+import {
+  Card,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+  Box,
+} from "@mui/material";
 
 function App() {
   const [books, setBooks] = useState([]);
@@ -21,32 +30,61 @@ function App() {
   return (
     <>
       <div> AMAZING BOOKS</div>
-      {books.map((book, i) => {
-        return (
-          <ul key={i}>
-            <div>Title: {book.title}</div>
-            <div>
-              Author:{" "}
-              {book.authors.map((author) => {
-                return author.name;
-              })}
-            </div>
-            <div>
-              Book themes:{" "}
-              {book.bookshelves
-                .filter((theme) => theme.startsWith("Browsing: "))
-                .map((theme) => theme.replace("Browsing: ", ""))
-                .join(", ")}
-            </div>
-            <div>
-              Number of downloads:{" "}
-              {book.download_count
-                .toString()
-                .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
-            </div>
-          </ul>
-        );
-      })}
+      <Box
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "center",
+        }}>
+        {books.map((book, i) => {
+          return (
+            <Card
+              sx={{
+                height: "100%",
+                width: "70%",
+                margin: 1,
+                backgroundColor: "lightblue",
+              }}>
+              <CardContent key={i}>
+                <Typography variant='h5' component='div'>
+                  {book.title}
+                </Typography>
+
+                <Typography
+                  variant='body2'
+                  sx={{ color: "text.secondary", fontSize: 14 }}>
+                  Author:{" "}
+                  {book.authors.map((author) => {
+                    return author.name;
+                  })}
+                </Typography>
+                <Typography sx={{ color: "text.secondary", mb: 1.5 }}>
+                  Book themes:{" "}
+                  {book.bookshelves
+                    .filter((theme) => theme.startsWith("Browsing: "))
+                    .map((theme) => theme.replace("Browsing: ", ""))
+                    .join(", ")}
+                </Typography>
+                <Typography variant='body2' sx={{ color: "text.secondary" }}>
+                  Number of downloads:{" "}
+                  {book.download_count
+                    .toString()
+                    .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                </Typography>
+              </CardContent>
+              <CardActions
+                sx={{
+                  display: "flex",
+                  flexWrap: "wrap",
+                  justifyContent: "center",
+                }}>
+                <Button size='small'>Share</Button>
+                <Button size='small'>Learn More</Button>
+              </CardActions>
+            </Card>
+          );
+        })}
+      </Box>
     </>
   );
 }
